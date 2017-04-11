7d1e2825d5fe7f72819dfc1490a4010ab806c45c
sanyoca <sanyoca@gmail.com>
2017-04-11 12:22:55 +0200

    Version 1.2
    - intents on clickable images

--- app/src/main/java/com/example/sanya/lighttravel/MainActivity.java
+++ app/src/main/java/com/example/sanya/lighttravel/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.sanya.lighttravel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.phonecall).setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:1115489772"));
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.geoloc).setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                String uri = String.format(Locale.ENGLISH, "geo:%f,%f", 47.523988, 19.038137);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(intent);            }
+        });
+
+        findViewById(R.id.email).setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"letstravel@universewaits.ea"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "Inquery about travelling possibilities");
+                try {
+                    startActivity(Intent.createChooser(i, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

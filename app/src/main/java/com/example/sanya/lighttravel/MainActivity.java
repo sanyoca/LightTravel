480c9d56c04a6e27014e29a7333cff8f0fbfa9d8
sanyoca <sanyoca@gmail.com>
2017-04-12 12:22:26 +0200

    Version 1.21
    - onclick is implemented instead of using new instances each time for the info textviews

--- app/src/main/java/com/example/sanya/lighttravel/MainActivity.java
+++ app/src/main/java/com/example/sanya/lighttravel/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,39 +17,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // start the phone call intent if that view is pressed
-        findViewById(R.id.phonecall).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:1115489772"));
-                startActivity(intent);
-            }
-        });
+        findViewById(R.id.phonecall).setOnClickListener(this);
 
         // start the maps intent if that view is pressed
-        findViewById(R.id.geoloc).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String uri = String.format(Locale.ENGLISH, "geo:%f,%f", 47.523988, 19.038137);
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                startActivity(intent);
-            }
-        });
+        findViewById(R.id.geoloc).setOnClickListener(this);
 
         // start the emailing intent if that view is pressed
-        findViewById(R.id.email).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("message/rfc822");
-                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"letstravel@universewaits.ea"});
-                i.putExtra(Intent.EXTRA_SUBJECT, "Inquery about travelling possibilities");
+        findViewById(R.id.email).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch(v.getId())   {
+            case R.id.phonecall:
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:1115489772"));
+                startActivity(intent);
+                break;
+            case R.id.geoloc:
+                String uri = String.format(Locale.ENGLISH, "geo:%f,%f", 47.523988, 19.038137);
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(intent);
+                break;
+            case R.id.email:
+                intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("message/rfc822");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"letstravel@universewaits.ea"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Inquery about travelling possibilities");
                 try {
-                    startActivity(Intent.createChooser(i, "Send mail..."));
+                    startActivity(Intent.createChooser(intent, "Send mail..."));
                 } catch (android.content.ActivityNotFoundException ex) {
                     Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
+                break;
+            default:
+                break;
+        }
     }
 }
